c7d44386295731cc72bfd56ebdbf721d446f82da
Nikola-Dronjak <nikola.s.dronjak@gmail.com>
2024-05-15 02:12:48 +0200

    Documented the OwnerRepository interface.

--- src/main/java/com/nikoladronjak/rently/repository/OwnerRepository.java
+++ src/main/java/com/nikoladronjak/rently/repository/OwnerRepository.java
@@ -7,8 +7,22 @@ import org.springframework.stereotype.Repository;
 
 import com.nikoladronjak.rently.domain.Owner;
 
+/**
+ * Represents a repository interface for accessing and managing Owner entities
+ * in the database. This interface extends the JpaRepository interface, which
+ * provides the basic CRUD operations for Owner entities.
+ * 
+ * @author Nikola Dronjak
+ */
 @Repository
 public interface OwnerRepository extends JpaRepository<Owner, Integer> {
 
+	/**
+	 * Retrieves an Owner entity by its email address.
+	 * 
+	 * @param email The email address of the owner being queried.
+	 * @return An Optional containing the Owner entity if found, or empty if not
+	 *         found.
+	 */
 	Optional<Owner> findByEmail(String email);
 }
