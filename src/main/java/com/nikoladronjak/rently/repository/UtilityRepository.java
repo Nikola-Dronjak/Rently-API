7bcc3440132e9e7518afc5d4f0725298caf1742d
Nikola-Dronjak <nikola.s.dronjak@gmail.com>
2024-05-15 02:14:48 +0200

    Documented the UtilityRepository interface.

--- src/main/java/com/nikoladronjak/rently/repository/UtilityRepository.java
+++ src/main/java/com/nikoladronjak/rently/repository/UtilityRepository.java
@@ -7,8 +7,22 @@ import org.springframework.stereotype.Repository;
 
 import com.nikoladronjak.rently.domain.Utility;
 
+/**
+ * Represents a repository interface for accessing and managing Utility entities
+ * in the database. This interface extends the JpaRepository interface, which
+ * provides the basic CRUD operations for Utility entities.
+ * 
+ * @author Nikola Dronjak
+ */
 @Repository
 public interface UtilityRepository extends JpaRepository<Utility, Integer> {
 
+	/**
+	 * Retrieves a Utility entity by its name.
+	 * 
+	 * @param name The name of the utility being queried.
+	 * @return An Optional containing the Utility entity if found, or empty if not
+	 *         found.
+	 */
 	Optional<Utility> findByName(String name);
 }
