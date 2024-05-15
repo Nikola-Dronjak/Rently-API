58cf789f29c1e0ae4f9b63b08dd150daeb103483
Nikola-Dronjak <nikola.s.dronjak@gmail.com>
2024-05-15 02:16:00 +0200

    Documented the OwnerService class.

--- src/main/java/com/nikoladronjak/rently/service/OwnerService.java
+++ src/main/java/com/nikoladronjak/rently/service/OwnerService.java
@@ -21,24 +21,54 @@ import com.nikoladronjak.rently.repository.OfficeSpaceRepository;
 import com.nikoladronjak.rently.repository.OwnerRepository;
 import com.nikoladronjak.rently.repository.ResidenceRepository;
 
+/**
+ * Represents a service class responsible for handling the business logic
+ * related to Owner entities. This class manages operations such as retrieval,
+ * adding, modification and deletion of Owner entities. Additionally, it
+ * supports conversion between Owner entities and OwnerDTOs.
+ * 
+ * @author Nikola Dronjak
+ */
 @Service
 public class OwnerService {
 
+	/**
+	 * Repository for accessing data related to residences.
+	 */
 	@Autowired
 	private ResidenceRepository residenceRepository;
 
+	/**
+	 * Repository for accessing data related to event spaces.
+	 */
 	@Autowired
 	private EventSpaceRepository eventSpaceRepository;
 
+	/**
+	 * Repository for accessing data related to office spaces.
+	 */
 	@Autowired
 	private OfficeSpaceRepository officeSpaceRepository;
 
+	/**
+	 * Repository for accessing data related to customers.
+	 */
 	@Autowired
 	private CustomerRepository customerRepository;
 
+	/**
+	 * Repository for accessing data related to owners.
+	 */
 	@Autowired
 	private OwnerRepository ownerRepository;
 
+	/**
+	 * Retrieves all owners from the database and converts them to OwnerDTOs.
+	 * 
+	 * @return ResponseEntity containing a list of OwnerDTOs if successful, or an
+	 *         error message with HttpStatus.INTERNAL_SERVER_ERROR status (500) if
+	 *         an exception occurs.
+	 */
 	public ResponseEntity<?> getAll() {
 		try {
 			List<Owner> owners = ownerRepository.findAll();
@@ -49,6 +79,16 @@ public class OwnerService {
 		}
 	}
 
+	/**
+	 * Retrieves an owner from the database by the specified id and converts it to
+	 * an OwnerDTO.
+	 * 
+	 * @param id The id of the owner that is being queried.
+	 * @return ResponseEntity containing the OwnerDTO if successful, or an error
+	 *         message with HttpStatus.BAD_REQUEST status (400) if an exception
+	 *         occurs.
+	 * @throws RuntimeException if there is no owner with the given id.
+	 */
 	public ResponseEntity<?> getById(Integer id) {
 		try {
 			Optional<Owner> ownerFromDb = ownerRepository.findById(id);
@@ -62,6 +102,17 @@ public class OwnerService {
 		}
 	}
 
+	/**
+	 * Adds a new owner to the database based on the provided OwnerDTO.
+	 * 
+	 * @param ownerDTO The OwnerDTO containing the details of the owner that is
+	 *                 being added.
+	 * @return ResponseEntity containing the newly created OwnerDTO if successful,
+	 *         or an error message with HttpStatus.BAD_REQUEST status (400) if an
+	 *         exception occurs.
+	 * @throws RuntimeException if the owner or customer with the provided email
+	 *                          already exists.
+	 */
 	public ResponseEntity<?> add(OwnerDTO ownerDTO) {
 		try {
 			if (ownerRepository.findByEmail(ownerDTO.getEmail()).isPresent()
@@ -77,6 +128,18 @@ public class OwnerService {
 		}
 	}
 
+	/**
+	 * Updates the owner information based on the provided id and OwnerDTO.
+	 * 
+	 * @param id       The id of the owner that is being updated.
+	 * @param ownerDTO The OwnerDTO containing the updated details of the owner.
+	 * @return ResponseEntity containing the updated OwnerDTO if successful, or an
+	 *         error message with HttpStatus.BAD_REQUEST status (400) if an
+	 *         exception occurs.
+	 * @throws RuntimeException if there is no owner with the given id, or if the
+	 *                          owner or customer with the provided email already
+	 *                          exists.
+	 */
 	public ResponseEntity<?> update(Integer id, OwnerDTO ownerDTO) {
 		try {
 			Optional<Owner> ownerFromDb = ownerRepository.findById(id);
@@ -104,6 +167,17 @@ public class OwnerService {
 		}
 	}
 
+	/**
+	 * Deletes the owner with the specified id.
+	 * 
+	 * @param id The id of the owner that is being deleted.
+	 * @return ResponseEntity containing the deleted OwnerDTO if successful, or an
+	 *         error message with HttpStatus.BAD_REQUEST status (400) if an
+	 *         exception occurs.
+	 * @throws RuntimeException if there is no owner with the given id, or if there
+	 *                          are properties associated with the owner, such as
+	 *                          residences, event spaces or office spaces.
+	 */
 	public ResponseEntity<?> delete(Integer id) {
 		try {
 			Optional<Owner> ownerFromDb = ownerRepository.findById(id);
@@ -125,6 +199,12 @@ public class OwnerService {
 		}
 	}
 
+	/**
+	 * Converts an Owner entity to an OwnerDTO.
+	 * 
+	 * @param owner The Owner entity that is being converted.
+	 * @return The corresponding OwnerDTO.
+	 */
 	private OwnerDTO convertToDTO(Owner owner) {
 		OwnerDTO ownerDTO = new OwnerDTO();
 		ownerDTO.setFirstName(owner.getFirstName());
@@ -136,6 +216,12 @@ public class OwnerService {
 		return ownerDTO;
 	}
 
+	/**
+	 * Converts an OwnerDTO to an Owner entity.
+	 * 
+	 * @param ownerDTO The OwnerDTO that is being converted.
+	 * @return The corresponding Owner entity.
+	 */
 	private Owner convertFromDTO(OwnerDTO ownerDTO) {
 		Owner owner = new Owner();
 		owner.setFirstName(ownerDTO.getFirstName());
