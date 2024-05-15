71fa8b43952adc72536844e776dd8dcfca8698ba
Nikola-Dronjak <nikola.s.dronjak@gmail.com>
2024-05-15 02:08:34 +0200

    Documented the OfficeSpace class.

--- src/main/java/com/nikoladronjak/rently/domain/OfficeSpace.java
+++ src/main/java/com/nikoladronjak/rently/domain/OfficeSpace.java
@@ -6,13 +6,35 @@ import java.util.stream.Collectors;
 
 import jakarta.persistence.*;
 
+/**
+ * Represents a domain class for storing information about a OfficeSpace entity.
+ * This class is mapped to the "OfficeSpaces" table in the database using JPA
+ * annotations. The primary key of this table is "propertyId". Since
+ * "propertyId" is inherited from the Property entity it also represents a
+ * foreign key that references the "Properties" table.
+ * 
+ * The OfficeSpace entity contains its capacity and a list of the utility leases
+ * associated with the OfficeSpace entity.
+ * 
+ * This entity is a child class of the Property class. This means that it also
+ * contains all the fields of the Property class.
+ * 
+ * @author Nikola Dronjak
+ */
 @Entity
 @Table(name = "OfficeSpaces")
 @PrimaryKeyJoinColumn(name = "propertyId")
 public class OfficeSpace extends Property {
 
+	/**
+	 * Represents the number of people that the office space can hold (int).
+	 */
 	private int capacity;
 
+	/**
+	 * Represents the list of utility leases with which the office space is
+	 * associated (List&lt;UtilityLease&gt;).
+	 */
 	@OneToMany(mappedBy = "property")
 	private List<UtilityLease> utilityLeases;
 
