d0e74421201ec79938f77b45d54cabcc951c52cb
Nikola-Dronjak <nikola.s.dronjak@gmail.com>
2024-05-15 02:12:09 +0200

    Documented the UtilityLeaseDTO class.

--- src/main/java/com/nikoladronjak/rently/dto/UtilityLeaseDTO.java
+++ src/main/java/com/nikoladronjak/rently/dto/UtilityLeaseDTO.java
@@ -5,14 +5,43 @@ import java.util.Objects;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
+/**
+ * Represents a data transfer object (DTO) for the UtilityLease entity. This
+ * class is used for transferring utility lease data between the different
+ * layers of the application (UtilityLeaseRepository, UtilityLeaseService and
+ * UtilityLeaseController). It plays a vital role in the creation of requests as
+ * well as the displaying of responses.
+ * 
+ * The UtilityLeaseDTO class contains the utilityId, propertyId and monthly
+ * rental rate of the utility lease.
+ * 
+ * @author Nikola Dronjak
+ */
 public class UtilityLeaseDTO {
 
+	/**
+	 * Represents the id of the utility that is being leased (Integer).
+	 * 
+	 * The id of the utility cannot be null.
+	 */
 	@NotNull(message = "You have to specify the utility which is being leased.")
 	private Integer utilityId;
 
+	/**
+	 * Represents the id of the property for which the utility is being leased
+	 * (Integer).
+	 * 
+	 * The id of the property cannot be null.
+	 */
 	@NotNull(message = "You have to specify the property for which the utility is being leased.")
 	private Integer propertyId;
 
+	/**
+	 * Represents the monthly rental rate for the utility lease (Double).
+	 * 
+	 * The rental rate cannot be null and it has to be a positive value (greater
+	 * than 0).
+	 */
 	@NotNull(message = "The rental rate of the utility being leased is required.")
 	@Positive(message = "The rental rate of the utility being leased has to be a positive value.")
 	private Double rentalRate;
