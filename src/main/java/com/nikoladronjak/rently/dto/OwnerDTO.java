8d8eb75f2ae9c79258680c7894212781a5f75712
Nikola-Dronjak <nikola.s.dronjak@gmail.com>
2024-05-15 02:10:15 +0200

    Documented the OwnerDTO class.

--- src/main/java/com/nikoladronjak/rently/dto/OwnerDTO.java
+++ src/main/java/com/nikoladronjak/rently/dto/OwnerDTO.java
@@ -6,24 +6,61 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+/**
+ * Represents a data transfer object (DTO) for the Owner entity. This class is
+ * used for transferring owner data between the different layers of the
+ * application (OwnerRepository, OwnerService and OwnerController). It plays a
+ * vital role in the creation of requests as well as the displaying of
+ * responses.
+ * 
+ * The OwnerDTO class contains the first name, last name, email address,
+ * password and the phone number of the owner.
+ * 
+ * @author Nikola Dronjak
+ */
 public class OwnerDTO {
 
+	/**
+	 * Represents the first name of the owner (String).
+	 * 
+	 * The first name cannot be null and it has to have at least 2 characters.
+	 */
 	@NotBlank(message = "The first name of the owner is required.")
 	@Size(min = 2, message = "The first name of the owner has to have at least 2 characters.")
 	private String firstName;
 
+	/**
+	 * Represents the last name of the owner (String).
+	 * 
+	 * The last name cannot be null and it has to have at least 2 characters.
+	 */
 	@NotBlank(message = "The last name of the owner is required.")
 	@Size(min = 2, message = "The last name of the owner has to have at least 2 characters.")
 	private String lastName;
 
+	/**
+	 * Represents the email address of the owner (String).
+	 * 
+	 * The email address cannot be null and it has to be a valid email address.
+	 */
 	@NotBlank(message = "The email address of the owner is required.")
 	@Email(message = "The email address of the owner must be valid.")
 	private String email;
 
+	/**
+	 * Represents the password of the owner (String).
+	 * 
+	 * The password cannot be null and it has to have at least 5 characters.
+	 */
 	@NotBlank(message = "The password of the owner is required.")
 	@Size(min = 5, message = "The password of the owner has to have at least 5 characters.")
 	private String password;
 
+	/**
+	 * Represents the phone number of the owner (String).
+	 * 
+	 * The phone number cannot be null and it has to have at least 10 characters.
+	 */
 	@NotBlank(message = "The phone number of the owner is required.")
 	@Size(min = 10, message = "The phone number of the owner has to have at least 10 characters.")
 	private String phoneNumber;
