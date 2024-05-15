573801937f4fb8ad54bc406212a639f2403834b4
Nikola-Dronjak <nikola.s.dronjak@gmail.com>
2024-05-15 02:13:45 +0200

    Documented the CustomerRepository interface.

--- src/main/java/com/nikoladronjak/rently/repository/CustomerRepository.java
+++ src/main/java/com/nikoladronjak/rently/repository/CustomerRepository.java
@@ -7,8 +7,22 @@ import org.springframework.stereotype.Repository;
 
 import com.nikoladronjak.rently.domain.Customer;
 
+/**
+ * Represents a repository interface for accessing and managing Customer
+ * entities in the database. This interface extends the JpaRepository interface,
+ * which provides the basic CRUD operations for Customer entities.
+ * 
+ * @author Nikola Dronjak
+ */
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
+	/**
+	 * Retrieves a Customer entity by its email address.
+	 * 
+	 * @param email The email address of the customer being queried.
+	 * @return An Optional containing the Customer entity if found, or empty if not
+	 *         found.
+	 */
 	Optional<Customer> findByEmail(String email);
 }
