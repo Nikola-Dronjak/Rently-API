bd1fae1ce04f44a147739b903c21b93eb7f34070
Nikola-Dronjak <nikola.s.dronjak@gmail.com>
2024-05-15 02:09:27 +0200

    Documented the Rent class.

--- src/main/java/com/nikoladronjak/rently/domain/Rent.java
+++ src/main/java/com/nikoladronjak/rently/domain/Rent.java
@@ -5,19 +5,54 @@ import java.util.Objects;
 
 import jakarta.persistence.*;
 
+/**
+ * Represents a domain class for storing information about a Rent entity. This
+ * class is mapped to the "Rents" table in the database using JPA annotations.
+ * The primary key of this table is "rentId". This table also contains a foreign
+ * key, "leaseId", which references the "Leases" table.
+ * 
+ * The Rent entity contains a rentId, a total (monthly) rental rate and a list
+ * of utility leases which are associated with the Rent entity.
+ * 
+ * @author Nikola Dronjak
+ */
 @Entity
 @Table(name = "Rents")
 public class Rent {
 
+	/**
+	 * Represents a unique identifier for the rent (int). This identifier is
+	 * generated automatically by JPA.
+	 */
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int rentId;
 
+	/**
+	 * Represents the total monthly rent (double). This rent is calculated by adding
+	 * up monthly rental rates for all the utility leases and the monthly rental
+	 * rate for the lease.
+	 */
 	private double totalRent;
 
+	/**
+	 * Represents the list of utility leases with which the rents are associated
+	 * (List&lt;UtilityLease&gt;).
+	 * 
+	 * This annotation creates an association table called "UtilityLeasesRents".
+	 * This table contains two columns:
+	 * <ul>
+	 * <li>rentId - The id of the rent associated with the utility lease.</li>
+	 * <li>utilityLeaseId - The id of the utility lease associated with the
+	 * rent.</li>
+	 * </ul>
+	 */
 	@ManyToMany(mappedBy = "rents", fetch = FetchType.EAGER)
 	private List<UtilityLease> utilityLeases;
 
+	/**
+	 * Represents the lease which is associated with the rent (Lease).
+	 */
 	@ManyToOne
 	@JoinColumn(name = "leaseId")
 	private Lease lease;
