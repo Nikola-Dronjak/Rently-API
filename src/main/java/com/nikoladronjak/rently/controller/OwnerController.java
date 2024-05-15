409f4e9efb423f28876f6d340c16d5b789e32c0d
Nikola-Dronjak <nikola.s.dronjak@gmail.com>
2024-05-15 02:17:52 +0200

    Documented the OwnerController class.

--- src/main/java/com/nikoladronjak/rently/controller/OwnerController.java
+++ src/main/java/com/nikoladronjak/rently/controller/OwnerController.java
@@ -24,41 +24,96 @@ import com.nikoladronjak.rently.service.OwnerService;
 
 import jakarta.validation.Valid;
 
+/**
+ * Represents a controller class for handling HTTP requests related to Owner
+ * entities. This class provides end-points for retrieving, adding, updating,
+ * and deleting Owner entities. It also handles validation errors by returning
+ * appropriate responses.
+ * 
+ * @author Nikola Dronjak
+ */
 @RestController
 @RequestMapping("/api/owners")
 public class OwnerController {
 
+	/**
+	 * Service for handling operations related to Owner entities.
+	 */
 	@Autowired
 	private OwnerService ownerService;
 
+	/**
+	 * Retrieves all owners. Route: HTTP GET /api/owners
+	 *
+	 * @return ResponseEntity with HTTP status and response body containing a list
+	 *         of owners or an error message.
+	 */
 	@GetMapping
 	public ResponseEntity<?> getAllOwners() {
 		return ownerService.getAll();
 	}
 
+	/**
+	 * Retrieves an owner by their id. Route: HTTP GET /api/owners/id
+	 *
+	 * @param id The id of the owner thats is being queried.
+	 * @return ResponseEntity with HTTP status and response body containing the
+	 *         owner if found, or an error message.
+	 */
 	@GetMapping("/{id}")
 	public ResponseEntity<?> getOwnerById(@PathVariable Integer id) {
 		return ownerService.getById(id);
 	}
 
+	/**
+	 * Adds a new owner. Route: HTTP POST /api/owners
+	 *
+	 * @param ownerDTO The OwnerDTO representing the owner that is being added.
+	 * @return ResponseEntity with HTTP status and response body containing the
+	 *         added owner if successful, or an error message.
+	 * @throws MethodArgumentNotValidException if the ownerDTO is not valid.
+	 */
 	@PostMapping
 	public ResponseEntity<?> addOwner(@Valid @RequestBody OwnerDTO ownerDTO) {
 		return ownerService.add(ownerDTO);
 	}
 
+	/**
+	 * Updates an existing owner. Route: HTTP PUT /api/owners/id
+	 *
+	 * @param id       The id of the owner that is being updated.
+	 * @param ownerDTO The OwnerDTO representing the updated owner information.
+	 * @return ResponseEntity with HTTP status and response body containing the
+	 *         updated owner if successful, or an error message.
+	 * @throws MethodArgumentNotValidException if the ownerDTO is not valid.
+	 */
 	@PutMapping("/{id}")
 	public ResponseEntity<?> updateOwner(@PathVariable Integer id, @Valid @RequestBody OwnerDTO ownerDTO) {
 		return ownerService.update(id, ownerDTO);
 	}
 
+	/**
+	 * Deletes an owner by their id. Route: HTTP DELETE /api/owners/id
+	 *
+	 * @param id The id of the owner that is being deleted.
+	 * @return ResponseEntity with HTTP status and response body containing the
+	 *         deleted owner if successful, or an error message.
+	 */
 	@DeleteMapping("/{id}")
 	public ResponseEntity<?> deleteOwner(@PathVariable Integer id) {
 		return ownerService.delete(id);
 	}
 
+	/**
+	 * Creates custom error messages for validation exceptions.
+	 *
+	 * @param e MethodArgumentNotValidException thrown during validation.
+	 * @return ResponseEntity with HTTP status and response body containing
+	 *         validation error details.
+	 */
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException e) {
+	private ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException e) {
 		Map<String, String> errors = new HashMap<String, String>();
 		e.getBindingResult().getAllErrors().forEach((error) -> {
 			String fieldName = ((FieldError) error).getField();
