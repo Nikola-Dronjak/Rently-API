73474db99041bc00f28838fd999080a9d531832a
Nikola-Dronjak <nikola.s.dronjak@gmail.com>
2024-05-15 02:18:24 +0200

    Documented the OfficeSpaceController class.

--- src/main/java/com/nikoladronjak/rently/controller/OfficeSpaceController.java
+++ src/main/java/com/nikoladronjak/rently/controller/OfficeSpaceController.java
@@ -24,42 +24,99 @@ import com.nikoladronjak.rently.service.OfficeSpaceService;
 
 import jakarta.validation.Valid;
 
+/**
+ * Represents a controller class for handling HTTP requests related to
+ * OfficeSpace entities. This class provides end-points for retrieving, adding,
+ * updating, and deleting OfficeSpace entities. It also handles validation
+ * errors by returning appropriate responses.
+ * 
+ * @author Nikola Dronjak
+ */
 @RestController
 @RequestMapping("/api/officespaces")
 public class OfficeSpaceController {
 
+	/**
+	 * Service for handling operations related to OfficeSpace entities.
+	 */
 	@Autowired
 	private OfficeSpaceService officeSpaceService;
 
+	/**
+	 * Retrieves all office spaces. Route: HTTP GET /api/officespaces
+	 *
+	 * @return ResponseEntity with HTTP status and response body containing a list
+	 *         of office spaces or an error message.
+	 */
 	@GetMapping
 	public ResponseEntity<?> getAllOfficeSpaces() {
 		return officeSpaceService.getAll();
 	}
 
+	/**
+	 * Retrieves a office space by its id. Route: HTTP GET /api/officespaces/id
+	 *
+	 * @param id The id of the office space thats is being queried.
+	 * @return ResponseEntity with HTTP status and response body containing the
+	 *         office space if found, or an error message.
+	 */
 	@GetMapping("/{id}")
 	public ResponseEntity<?> getOfficeSpaceById(@PathVariable Integer id) {
 		return officeSpaceService.getById(id);
 	}
 
+	/**
+	 * Adds a new office space. Route: HTTP POST /api/officespaces
+	 *
+	 * @param officeSpaceDTO The OfficeSpaceDTO representing the office space that
+	 *                       is being added.
+	 * @return ResponseEntity with HTTP status and response body containing the
+	 *         added office space if successful, or an error message.
+	 * @throws MethodArgumentNotValidException if the officeSpaceDTO is not valid.
+	 */
 	@PostMapping
 	public ResponseEntity<?> addOfficeSpace(@Valid @RequestBody OfficeSpaceDTO officeSpaceDTO) {
 		return officeSpaceService.add(officeSpaceDTO);
 	}
 
+	/**
+	 * Updates an existing office space. Route: HTTP PUT /api/officespaces/id
+	 *
+	 * @param id             The id of the office space that is being updated.
+	 * @param officeSpaceDTO The OfficeSpaceDTO representing the updated office
+	 *                       space information.
+	 * @return ResponseEntity with HTTP status and response body containing the
+	 *         updated office space if successful, or an error message.
+	 * @throws MethodArgumentNotValidException if the officeSpaceDTO is not valid.
+	 */
 	@PutMapping("/{id}")
 	public ResponseEntity<?> updateOfficeSpace(@PathVariable Integer id,
 			@Valid @RequestBody OfficeSpaceDTO officeSpaceDTO) {
 		return officeSpaceService.update(id, officeSpaceDTO);
 	}
 
+	/**
+	 * Deletes a office space by its id. Route: HTTP DELETE /api/officespaces/id
+	 *
+	 * @param id The id of the office space that is being deleted.
+	 * @return ResponseEntity with HTTP status and response body containing the
+	 *         deleted office space if successful, or an error message.
+	 */
 	@DeleteMapping("/{id}")
 	public ResponseEntity<?> deleteOfficeSpace(@PathVariable Integer id) {
 		return officeSpaceService.delete(id);
 	}
 
+	/**
+	 * Creates custom error messages for validation exceptions.
+	 *
+	 * @param e MethodArgumentNotValidException thrown during validation.
+	 * @return ResponseEntity with HTTP status and response body containing
+	 *         validation error details.
+	 */
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException e) {
+	private ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException e) {
 		Map<String, String> errors = new HashMap<String, String>();
 		e.getBindingResult().getAllErrors().forEach((error) -> {
 			String fieldName = ((FieldError) error).getField();
