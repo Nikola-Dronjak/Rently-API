d69484ee319ff2549cf92545e49fb4a930be1565
Nikola-Dronjak <nikola.s.dronjak@gmail.com>
2024-05-15 02:10:23 +0200

    Documented the CustomerDTO class.

--- src/main/java/com/nikoladronjak/rently/dto/CustomerDTO.java
+++ src/main/java/com/nikoladronjak/rently/dto/CustomerDTO.java
@@ -6,20 +6,52 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+/**
+ * Represents a data transfer object (DTO) for the Customer entity. This class
+ * is used for transferring customer data between the different layers of the
+ * application (CustomerRepository, CustomerService and CustomerController). It
+ * plays a vital role in the creation of requests as well as the displaying of
+ * responses.
+ * 
+ * The CustomerDTO class contains the first name, last name, email address and
+ * password of the customer.
+ * 
+ * @author Nikola Dronjak
+ */
 public class CustomerDTO {
 
+	/**
+	 * Represents the first name of the customer (String).
+	 * 
+	 * The first name cannot be null and it has to have at least 2 characters.
+	 */
 	@NotBlank(message = "The first name of the customer is required.")
 	@Size(min = 2, message = "The first name of the customer has to have at least 2 characters.")
 	private String firstName;
 
+	/**
+	 * Represents the last name of the customer (String).
+	 * 
+	 * The last name cannot be null and it has to have at least 2 characters.
+	 */
 	@NotBlank(message = "The last name of the customer is required.")
 	@Size(min = 2, message = "The last name of the customer has to have at least 2 characters.")
 	private String lastName;
 
+	/**
+	 * Represents the email address of the customer (String).
+	 * 
+	 * The email address cannot be null and it has to be a valid email address.
+	 */
 	@NotBlank(message = "The email address of the customer is required.")
 	@Email(message = "The email address of the customer must be valid.")
 	private String email;
 
+	/**
+	 * Represents the password of the customer (String).
+	 * 
+	 * The password cannot be null and it has to have at least 5 characters.
+	 */
 	@NotBlank(message = "The password of the customer is required.")
 	@Size(min = 5, message = "The password of the customer has to have at least 5 characters.")
 	private String password;
