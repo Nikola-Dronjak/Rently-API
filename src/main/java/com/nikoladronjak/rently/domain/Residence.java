283feb5a7fafa2158363201dd62fa1c0bd12fdf1
Nikola-Dronjak <nikola.s.dronjak@gmail.com>
2024-05-15 02:08:07 +0200

    Documented the Residence class.

--- src/main/java/com/nikoladronjak/rently/domain/Residence.java
+++ src/main/java/com/nikoladronjak/rently/domain/Residence.java
@@ -6,19 +6,64 @@ import java.util.stream.Collectors;
 
 import jakarta.persistence.*;
 
+/**
+ * Represents a domain class for storing information about a Residence entity.
+ * This class is mapped to the "Residences" table in the database using JPA
+ * annotations. The primary key of this table is "propertyId". Since
+ * "propertyId" is inherited from the Property entity it also represents a
+ * foreign key that references the "Properties" table.
+ * 
+ * The Residence entity contains the number of bedrooms, the number of
+ * bathrooms, an isPetFriendly flag and an isFurnished flag.
+ * 
+ * This entity is a child class of the Property class. This means that it also
+ * contains all the fields of the Property class.
+ * 
+ * @author Nikola Dronjak
+ */
 @Entity
 @Table(name = "Residences")
 @PrimaryKeyJoinColumn(name = "propertyId")
 public class Residence extends Property {
 
+	/**
+	 * Represents the number of bedrooms in a residence (int).
+	 */
 	private int numberOfBedrooms;
 
+	/**
+	 * Represents the number of bathrooms in a residence (int).
+	 */
 	private int numberOfBathrooms;
 
+	/**
+	 * Represents the heating type of the residence (HeatingType). There are 4
+	 * different types of heating:
+	 * <ul>
+	 * <li>Central heating</li>
+	 * <li>Gas heating</li>
+	 * <li>Electrical heating</li>
+	 * <li>Wood heating</li>
+	 * </ul>
+	 */
 	private HeatingType heatingType;
 
+	/**
+	 * Indicates whether the residence is pet-friendly (boolean).
+	 * <ul>
+	 * <li>True - The residence is pet-friendly.</li>
+	 * <li>False - The residence is not pet-friendly.</li>
+	 * </ul>
+	 */
 	private boolean isPetFriendly;
 
+	/**
+	 * Indicates whether the residence is furnished (boolean).
+	 * <ul>
+	 * <li>True - The residence is furnished.</li>
+	 * <li>False - The residence is not furnished.</li>
+	 * </ul>
+	 */
 	private boolean isFurnished;
 
 	public Residence() {
