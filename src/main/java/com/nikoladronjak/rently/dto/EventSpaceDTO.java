bb2f93a45c68f400539abd4c8a11c808e053ed67
Nikola-Dronjak <nikola.s.dronjak@gmail.com>
2024-05-15 02:11:19 +0200

    Documented the EventSpaceDTO class.

--- src/main/java/com/nikoladronjak/rently/dto/EventSpaceDTO.java
+++ src/main/java/com/nikoladronjak/rently/dto/EventSpaceDTO.java
@@ -6,18 +6,59 @@ import java.util.Objects;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
+/**
+ * Represents a data transfer object (DTO) for the EventSpace entity. This class
+ * is used for transferring event space data between the different layers of the
+ * application (EventSpaceRepository, EventSpaceService and
+ * EventSpaceController). It plays a vital role in the creation of requests as
+ * well as the displaying of responses.
+ * 
+ * The EventSpaceDTO class contains the capacity, hasKitchen flag, hasBar flag
+ * and ownerId.
+ * 
+ * @author Nikola Dronjak
+ */
 public class EventSpaceDTO extends PropertyDTO {
 
+	/**
+	 * Represents the number of people that the event space can hold (Integer).
+	 * 
+	 * The capacity cannot be null and it has to be a positive value (greater than
+	 * 0).
+	 */
 	@NotNull(message = "The size of the event space is required.")
 	@Positive(message = "The size of the event space has to be a positive value.")
 	private Integer capacity;
 
+	/**
+	 * Indicates whether the event space has a kitchen (Boolean).
+	 * <ul>
+	 * <li>True - The event space has a kitchen.</li>
+	 * <li>False - The event space doesn't have a kitchen.</li>
+	 * </ul>
+	 * 
+	 * The hasKitchen flag cannot be null.
+	 */
 	@NotNull(message = "You have to specify whether the event space has a kitchen or not.")
 	private Boolean hasKitchen;
 
+	/**
+	 * Indicates whether the event space has a bar (Boolean).
+	 * <ul>
+	 * <li>True - The event space has a bar.</li>
+	 * <li>False - The event space doesn't have a bar.</li>
+	 * </ul>
+	 * 
+	 * The hasBar flag cannot be null.
+	 */
 	@NotNull(message = "You have to specify whether the event space has a bar or not.")
 	private Boolean hasBar;
 
+	/**
+	 * Represents the id of the event space's owner (Integer).
+	 * 
+	 * The id of the owner cannot be null.
+	 */
 	@NotNull(message = "You have to specify the owner of the event space.")
 	private Integer ownerId;
 
