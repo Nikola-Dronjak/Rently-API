4eef7d75b7b31346c8d03718952f5c7b1f308ad2
Nikola-Dronjak <nikola.s.dronjak@gmail.com>
2024-05-15 02:09:45 +0200

    Documented the HeatingType enumeration.

--- src/main/java/com/nikoladronjak/rently/domain/HeatingType.java
+++ src/main/java/com/nikoladronjak/rently/domain/HeatingType.java
@@ -1,5 +1,23 @@
 package com.nikoladronjak.rently.domain;
 
+/**
+ * Represents different heating types for a residence.
+ */
 public enum HeatingType {
-	Central, Gas, Electricity, Wood
+	/**
+	 * Represents the central heating system.
+	 */
+	Central,
+	/**
+	 * Represents the gas heating system.
+	 */
+	Gas,
+	/**
+	 * Represents the electrical heating system.
+	 */
+	Electricity,
+	/**
+	 * Represents the wood heating system(furnace).
+	 */
+	Wood
 }
