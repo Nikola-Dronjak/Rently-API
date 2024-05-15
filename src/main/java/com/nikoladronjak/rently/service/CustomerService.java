bb0de8595f2bcb646e7125611c2177e9b23e8497
Nikola-Dronjak <nikola.s.dronjak@gmail.com>
2024-05-15 02:16:08 +0200

    Documented the CustomerService class.

--- src/main/java/com/nikoladronjak/rently/service/CustomerService.java
+++ src/main/java/com/nikoladronjak/rently/service/CustomerService.java
@@ -17,18 +17,43 @@ import com.nikoladronjak.rently.repository.CustomerRepository;
 import com.nikoladronjak.rently.repository.LeaseRepository;
 import com.nikoladronjak.rently.repository.OwnerRepository;
 
+/**
+ * Represents a service class responsible for handling the business logic
+ * related to Customer entities. This class manages operations such as
+ * retrieval, adding, modification and deletion of Customer entities.
+ * Additionally, it supports conversion between Customer entities and
+ * CustomerDTOs.
+ * 
+ * @author Nikola Dronjak
+ */
 @Service
 public class CustomerService {
 
+	/**
+	 * Repository for accessing data related to owners.
+	 */
 	@Autowired
 	private OwnerRepository ownerRepository;
 
+	/**
+	 * Repository for accessing data related to leases.
+	 */
 	@Autowired
 	private LeaseRepository leaseRepository;
 
+	/**
+	 * Repository for accessing data related to customers.
+	 */
 	@Autowired
 	private CustomerRepository customerRepository;
 
+	/**
+	 * Retrieves all customers from the database and converts them to CustomerDTOs.
+	 * 
+	 * @return ResponseEntity containing a list of CustomerDTOs if successful, or an
+	 *         error message with HttpStatus.INTERNAL_SERVER_ERROR status (500) if
+	 *         an exception occurs.
+	 */
 	public ResponseEntity<?> getAll() {
 		try {
 			List<Customer> customers = customerRepository.findAll();
@@ -39,6 +64,16 @@ public class CustomerService {
 		}
 	}
 
+	/**
+	 * Retrieves a customer from the database by the specified id and converts it to
+	 * a CustomerDTO.
+	 * 
+	 * @param id The id of the customer that is being queried.
+	 * @return ResponseEntity containing the CustomerDTO if successful, or an error
+	 *         message with HttpStatus.BAD_REQUEST status (400) if an exception
+	 *         occurs.
+	 * @throws RuntimeException if there is no customer with the given id.
+	 */
 	public ResponseEntity<?> getById(Integer id) {
 		try {
 			Optional<Customer> customerFromDb = customerRepository.findById(id);
@@ -52,6 +87,17 @@ public class CustomerService {
 		}
 	}
 
+	/**
+	 * Adds a new customer to the database based on the provided CustomerDTO.
+	 * 
+	 * @param customerDTO The CustomerDTO containing the details of the customer
+	 *                    that is being added.
+	 * @return ResponseEntity containing the newly created CustomerDTO if
+	 *         successful, or an error message with HttpStatus.BAD_REQUEST status
+	 *         (400) if an exception occurs.
+	 * @throws RuntimeException if the customer or owner with the provided email
+	 *                          already exists.
+	 */
 	public ResponseEntity<?> add(CustomerDTO customerDTO) {
 		try {
 			if (customerRepository.findByEmail(customerDTO.getEmail()).isPresent()
@@ -67,6 +113,19 @@ public class CustomerService {
 		}
 	}
 
+	/**
+	 * Updates the customer information based on the provided id and CustomerDTO.
+	 * 
+	 * @param id          The id of the customer that is being updated.
+	 * @param customerDTO The CustomerDTO containing the updated details of the
+	 *                    customer.
+	 * @return ResponseEntity containing the updated CustomerDTO if successful, or
+	 *         an error message with HttpStatus.BAD_REQUEST status (400) if an
+	 *         exception occurs.
+	 * @throws RuntimeException if there is no customer with the given id, or if the
+	 *                          customer or owner with the provided email already
+	 *                          exists.
+	 */
 	public ResponseEntity<?> update(Integer id, CustomerDTO customerDTO) {
 		try {
 			Optional<Customer> customerFromDb = customerRepository.findById(id);
@@ -94,6 +153,16 @@ public class CustomerService {
 		}
 	}
 
+	/**
+	 * Deletes the customer with the specified id.
+	 * 
+	 * @param id The id of the customer that is being deleted.
+	 * @return ResponseEntity containing the deleted CustomerDTO if successful, or
+	 *         an error message with HttpStatus.BAD_REQUEST status (400) if an
+	 *         exception occurs.
+	 * @throws RuntimeException if there is no customer with the given id, or if
+	 *                          there are leases associated with the customer.
+	 */
 	public ResponseEntity<?> delete(Integer id) {
 		try {
 			Optional<Customer> customerFromDb = customerRepository.findById(id);
@@ -113,6 +182,12 @@ public class CustomerService {
 		}
 	}
 
+	/**
+	 * Converts a Customer entity to a CustomerDTO.
+	 * 
+	 * @param customer The Customer entity that is being converted.
+	 * @return The corresponding CustomerDTO.
+	 */
 	private CustomerDTO convertToDTO(Customer customer) {
 		CustomerDTO customerDTO = new CustomerDTO();
 		customerDTO.setFirstName(customer.getFirstName());
@@ -123,6 +198,12 @@ public class CustomerService {
 		return customerDTO;
 	}
 
+	/**
+	 * Converts a CustomerDTO to a Customer entity.
+	 * 
+	 * @param customerDTO The CustomerDTO that is being converted.
+	 * @return The corresponding Customer entity.
+	 */
 	private Customer convertFromDTO(CustomerDTO customerDTO) {
 		Customer customer = new Customer();
 		customer.setFirstName(customerDTO.getFirstName());
