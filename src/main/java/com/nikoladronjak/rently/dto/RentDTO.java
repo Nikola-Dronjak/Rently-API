f25cb84ee60b5ae0698fea0755bd6c55a865eb2a
Nikola-Dronjak <nikola.s.dronjak@gmail.com>
2024-05-15 02:12:20 +0200

    Documented the RentDTO class.

--- src/main/java/com/nikoladronjak/rently/dto/RentDTO.java
+++ src/main/java/com/nikoladronjak/rently/dto/RentDTO.java
@@ -5,13 +5,42 @@ import java.util.Objects;
 
 import jakarta.validation.constraints.NotNull;
 
+/**
+ * Represents a data transfer object (DTO) for the Rent entity. This class is
+ * used for transferring rent data between the different layers of the
+ * application (RentRepository, RentService and RentController). It plays a
+ * vital role in the creation of requests as well as the displaying of
+ * responses.
+ * 
+ * The RentDTO class contains the leaseId, total monthly rent and list of the
+ * utility lease ids.
+ * 
+ * @author Nikola Dronjak
+ */
 public class RentDTO {
 
+	/**
+	 * Represents the id of the lease for which the rent is being calculated
+	 * (Integer).
+	 * 
+	 * The id of the lease cannot be null.
+	 */
 	@NotNull(message = "You have to specify the lease from which the rent is derived.")
 	private Integer leaseId;
 
+	/**
+	 * Represents the total monthly rent (Double). This rent is calculated by adding
+	 * up monthly rental rates for all the utility leases and the monthly rental
+	 * rate for the lease.
+	 */
 	private Double totalRent;
 
+	/**
+	 * Represents the list of all the utility leases that are being leased
+	 * (List&lt;Integer&gt;).
+	 * 
+	 * The list of utility lease ids cannot be null.
+	 */
 	@NotNull(message = "You have to specify the utility leases which are part of the rent.")
 	private List<Integer> utilityLeaseIds;
 
